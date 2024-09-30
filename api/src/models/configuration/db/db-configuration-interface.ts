999e67db796ae2de085cf80990a7fcf544bc0742
Kevin Chen <kevchen21@gmail.com>
2024-09-29 18:50:23 -0700

    fix permissions

--- api/src/models/configuration/db/db-configuration-interface.ts
+++ api/src/models/configuration/db/db-configuration-interface.ts
@@ -2,6 +2,7 @@ export interface IDbConfiguration {
     readonly dbRegion: string;
     readonly tableName: string;
     readonly endpoint: string;
+    readonly dbIndexName: string;
 }
 
 export const IDbConfiguration = Symbol.for("IDbConfiguration");
--- api/src/models/configuration/db/db-configuration.ts
+++ api/src/models/configuration/db/db-configuration.ts
@@ -7,13 +7,28 @@ export class DbConfiguration implements IDbConfiguration {
     readonly dbRegion: string;
     readonly tableName: string;
     readonly endpoint: string;
+    readonly dbIndexName: string;
 
     constructor() {
         assert(!!process.env.dbRegion, "DbRegion was undefined");
         assert(!!process.env.dbTableName, "DbEndpoint was undefined");
         assert(!!process.env.dbEndpoint, "DbTableName was undefined");
+        assert(!!process.env.dbIndexName, "DbIndexName was undefined");
+
+
         this.dbRegion = process.env.dbRegion;
-        this.tableName = process.env.dbTableName;
+        this.tableName = this.formatResourceName(process.env.dbTableName);
         this.endpoint = process.env.dbEndpoint;
+        this.dbIndexName = this.formatResourceName(process.env.dbIndexName, process.env.dbTableName);
+    }
+
+    private formatResourceName(resourceName: string, associatedTableName: string = undefined): string {
+        if (process.env.AwsAccountId !== process.env.ResourceAccountId) {
+            return associatedTableName === undefined
+                ? `arn:aws:dynamodb:${process.env.dbRegion}:${process.env.ResourceAccountId}:table/${resourceName}`
+                : `arn:aws:dynamodb:${process.env.dbRegion}:${process.env.ResourceAccountId}:table/${associatedTableName}/index/${resourceName}`;
+        }
+
+        return resourceName;
     }
 }
